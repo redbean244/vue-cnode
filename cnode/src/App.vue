a9dcf5125968d742dad6e6649b3009191048d889
redbean244 <798388771@qq.com>
2019-04-02 15:59:00 +0800

    Headerr组件

---
+++ cnode/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header'
+  export default {
+    name: 'App',
+    components: {
+      Header
+    }
+  }
+</script>
+
+<style>
+#app {
+  
+}
+</style>
